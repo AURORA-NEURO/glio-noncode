25dceee2b9342f58b2c7164ad072a799e23292e4
MurariAmbati <170054497+MurariAmbati@users.noreply.github.com>
2026-09-21 07:36:07 -0500

    Add D308 runtime evaluation

--- src/glio_noncode/_public_surface.py
+++ src/glio_noncode/_public_surface.py
@@ -4,7 +4,7 @@
 MANIFEST_VERSION = 2
 SOURCE_DIGEST = '8a20fb8d562bce76b17bdf17736380dd849b1a265b13769ef68d9bdba9ac7296'
 ALL_DIGEST = 'c2290596c1986d9bdd397cdb04e8eeec43540fa1dc6f856f0e13e28f52e1984f'
-SURFACE_DIGEST = 'a4d68b6e1eb98f241b7daf84361650a9eb50f87fb332450435bc76e11cd1a822'
+SURFACE_DIGEST = '6f57de6e90d359e8de1af5f2c2a15182216523609953d29b5455707d67068bda'
 
 ALL = (
     'CaseManifest',
@@ -42824,6 +42824,10 @@ CHILD_MODULES = {
     'downloaded_data_quality_d307_history_diff_audit': 'glio_noncode.downloaded_data_quality_d307_history_diff_audit',
     'downloaded_data_quality_d307_history_diff_query': 'glio_noncode.downloaded_data_quality_d307_history_diff_query',
     'downloaded_data_quality_d307_history_diff_query_audit': 'glio_noncode.downloaded_data_quality_d307_history_diff_query_audit',
+    'downloaded_data_quality_d308_history_diff_runtime': 'glio_noncode.downloaded_data_quality_d308_history_diff_runtime',
+    'downloaded_data_quality_d308_history_diff_runtime_audit': 'glio_noncode.downloaded_data_quality_d308_history_diff_runtime_audit',
+    'downloaded_data_quality_d308_history_diff_runtime_query': 'glio_noncode.downloaded_data_quality_d308_history_diff_runtime_query',
+    'downloaded_data_quality_d308_history_diff_runtime_query_audit': 'glio_noncode.downloaded_data_quality_d308_history_diff_runtime_query_audit',
     'downloaded_data_quality_diff': 'glio_noncode.downloaded_data_quality_diff',
     'downloaded_data_quality_diff_audit': 'glio_noncode.downloaded_data_quality_diff_audit',
     'downloaded_data_quality_diff_gate': 'glio_noncode.downloaded_data_quality_diff_gate',
@@ -47348,6 +47352,10 @@ LAZY_MODULES = (
     'glio_noncode.downloaded_data_quality_d307_history_diff_audit',
     'glio_noncode.downloaded_data_quality_d307_history_diff_query',
     'glio_noncode.downloaded_data_quality_d307_history_diff_query_audit',
+    'glio_noncode.downloaded_data_quality_d308_history_diff_runtime',
+    'glio_noncode.downloaded_data_quality_d308_history_diff_runtime_audit',
+    'glio_noncode.downloaded_data_quality_d308_history_diff_runtime_query',
+    'glio_noncode.downloaded_data_quality_d308_history_diff_runtime_query_audit',
     'glio_noncode.downloaded_data_quality_diff',
     'glio_noncode.downloaded_data_quality_diff_audit',
     'glio_noncode.downloaded_data_quality_diff_gate',
